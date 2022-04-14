fe9dfda853ed1d3b3bd71558579549848e093ad0
Daniel <danielnun9719@gmail.com>
2022-04-13 22:32:05 -0500

    Se eliminaron los json de data

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
             <a rel="noreferrer" className="icon-contact" href="https://www.instagram.com/_dannsan/" target="_blank" ><i className="fa-brands fa-instagram"></i></a> 
             <a rel="noreferrer" className="icon-contact" href="https://www.linkedin.com/in/alan-daniel-nu%C3%B1ez/" target="_blank" ><i className="fa-brands fa-linkedin-in"></i></a> 
             <a rel="noreferrer" className="icon-contact" href="https://github.com/AlanDannS" target="_blank" ><i className="fa-brands fa-github"></i></a> 
-            <a rel="noreferrer" className="icon-contact" href="https://www.freecodecamp.org/DannS" target="_blank" ><i className="fa-brands fa-free-code-camp"></i></a> 
+            <a rel="noreferrer" className="icon-contact" href="mailto:danielnun9719@gmail.com" target="_blank" ><i className="fa-solid fa-envelope"></i></a> 
           </div>
 </Fade>
 </section>
